6aaa6ddd23a60e87c107718f768d0339bb5b925a
Patryk <patryk.szusbier@gmail.com>
2022-06-27 20:49:30 +0200

    Dodanie nowych klas oraz dodanie możliwości dodawania wszystkich klas i wyświetlanie wszystkiego w bazie danych

--- Warehouse/Class.cpp
+++ Warehouse/Class.cpp
@@ -8,10 +8,16 @@
 
 using namespace std;
 
+int prid = 1,grid=1,ramid=1,motid=1,powid=1,haid=1, prodid = 1;
+const int zero = 0;
+vector <Producent> producenci;
 vector <Processor> procesory;
-vector <Graphic_Card> karty;
+vector <GraphicCards> karty;
+vector <RAM_memory> ramy;
+vector <Motherboard> plyty;
+vector <Power_Supply> zasilacze;
+vector <Hard_Drive> dyski;
 
-int pid = 0,prodid = 0;
 //--------------------------------------------------------------
 //|                     Klasa Computer_Parts                   |
 //--------------------------------------------------------------
@@ -30,25 +36,42 @@ int Computer_Parts::get_id()
 //--------------------------------------------------------------
 //|                     Klasa Processor                        |
 //--------------------------------------------------------------
-Processor::Processor(string n , int i, double MHz,int amm) :Computer_Parts(i)
+Processor::Processor(string n , int i, float MHz,int amm,int idpr) :Computer_Parts(i)
 {
     name = n;
-    MHz = MHz;
+    this->MHz = MHz;
     amount = amm;
-    
+    this->idpr = idpr;
 }
-//==================================================Do zmiany============================================
-
 void Processor::show_all()
 {
-    Computer_Parts::show(); 
-    cout<< " " << name << "   " << MHz <<endl;
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (idpr == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << " " << name << "   " << MHz << "   " << amount << endl;
 }
 void Processor::show_min()
 {
-
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (idpr == a)
+        {
+            producenci[i].gname();
+        }
+    }
+        cout << "  " << name << " "<<amount << endl;
 }
-string Processor::namee()
+string Processor::get_name()
 {
     return name;
 }
@@ -59,52 +82,345 @@ int Processor::amm()
 float Processor::M()
 {
     return MHz;
+
 }
-int Processor::idd()
+int Processor::get_idpr()
 {
-
-    return Computer_Parts::get_id();
-
+    return idpr;
+}
+void Processor::add_amm(int t)
+{
+    amount = amount + t;
+}
+void Processor::rem_amm(int t)
+{
+    if (amount = amount - t < 0)
+    {
+        amount = 0;
+    }
+    else
+    {
+        amount = amount - t;
+    }
+    
 }
 //--------------------------------------------------------------
 //|                     Klasa Graphic_Card                     |
 //--------------------------------------------------------------
-Graphic_Card::Graphic_Card(string n ,int rr,int id,int am) :Computer_Parts(id)
+GraphicCards::GraphicCards(string n ,int rr,int id,int am, int id2) :Computer_Parts(id)
 {
     name = n;
     Vram = rr;
     amount = am;
+    idp = id2;
+    
 }
-void Graphic_Card::show()
+void GraphicCards::show()
 {
-    cout << "Id produktu: ";
+
     Computer_Parts::show();
+    cout << "Karta Graficzna " << name << " " << Vram << " GB" << "  ";
+    Producent::gname();
     cout << endl;
-    cout << "Karta Graficzna " << name << " " << Vram << " GB" << endl;
 }
-void Graphic_Card::save()
+void GraphicCards::show_all()
 {
-   
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (idp == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << " " << name << "   " << Vram << "   " << amount << endl;
 }
-void Graphic_Card::open()
+void GraphicCards::show_min()
 {
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (idp == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << " " << amount << endl;
+}
+string GraphicCards::get_name()
+{
+    return name;
+}
+int GraphicCards::get_Vram()
+{
+    return Vram;
+}
+int GraphicCards::get_am()
+{
+    return amount;
+}
+void GraphicCards::add_amm(int t)
+{
+    amount = amount + t;
+}
+void GraphicCards::rem_amm(int t)
+{
+    if (amount = amount - t < 0)
+    {
+        amount = 0;
+    }
+    else
+    {
+        amount = amount - t;
+    }
 
 }
 //--------------------------------------------------------------
 //|                     Klasa RAM_memory                       |
 //--------------------------------------------------------------
+RAM_memory::RAM_memory(string nam, int g, int T, int id, int idp) : Computer_Parts(id)
+{
+    name = nam;
+    GB = g;
+    Takt = T;
+    prod = idp;
+}
+void RAM_memory::show_all()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << " " << name << "   " << GB << "   " <<Takt<<"  "<< amount << endl;
+}
+void RAM_memory::show_min()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << " " << amount << endl;
+}
+void RAM_memory::add_amm(int t)
+{
+    amount = amount + t;
+}
+void RAM_memory::rem_amm(int t)
+{
+    if (amount = amount - t < 0)
+    {
+        amount = 0;
+    }
+    else
+    {
+        amount = amount - t;
+    }
 
-//--------------------------------------------------------------
+}
+//-------------------------------------------------------------
 //|                     Klasa Motherboard                      |
 //--------------------------------------------------------------
+Motherboard::Motherboard(string nam, string forr, string soc,int amo,int id1,int id2):Computer_Parts(id1)
+{
+    name = nam;
+    format = forr;
+    socket = soc;
+    amount = amo;
+    prod = id2;
+}
+void Motherboard::show_all()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << " " << name << "   " << format << "  "<<socket<<"   " << amount << endl;
+}
+void Motherboard::show_min()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << " " << amount << endl;
+}
+void Motherboard::add_amm(int t)
+{
+    amount = amount + t;
+}
+void Motherboard::rem_amm(int t)
+{
+    if (amount = amount - t < 0)
+    {
+        amount = 0;
+    }
+    else
+    {
+        amount = amount - t;
+    }
 
+}
 //--------------------------------------------------------------
 //|                     Klasa Power_Supply                     |
 //--------------------------------------------------------------
+Power_Supply::Power_Supply(string nam, int Pow, int am, int pid, int id1) :Computer_Parts(id1)
+{
+    name = nam;
+    Power = Pow;
+    amount = am;
+    prod = pid;
+}
+void Power_Supply::show_all()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << " "<<"  "<<Power<<"W   " << amount << endl;
+}
+void Power_Supply::show_min()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << " " << amount << endl;
+}
+void Power_Supply::add_amm(int t)
+{
+    amount = amount + t;
+}
+void Power_Supply::rem_amm(int t)
+{
+    if (amount = amount - t < 0)
+    {
+        amount = 0;
+    }
+    else
+    {
+        amount = amount - t;
+    }
+
+}
+//--------------------------------------------------------------
+//|                     Klasa Hard_Drive                       |
+//--------------------------------------------------------------
+Hard_Drive::Hard_Drive(string nam, int po, int id1, int pid, int am) : Computer_Parts(id1)
+{
+    name = nam;
+    poj = po;
+    prod = pid;
+}
+void Hard_Drive::show_all()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << " " << "  " << poj << "GB   " << amount << endl;
+}
+void Hard_Drive::show_min()
+{
+    Computer_Parts::show();
+    cout << "  ";
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        int a = producenci[i].get_id();
+        if (prod == a)
+        {
+            producenci[i].gname();
+        }
+    }
+    cout << "  " << name << "  " << amount << endl;
+}
+void Hard_Drive::add_amm(int t)
+{
+    amount = amount + t;
+}
+void Hard_Drive::rem_amm(int t)
+{
+    if (amount = amount - t < 0)
+    {
+        amount = 0;
+    }
+    else
+    {
+        amount = amount - t;
+    }
+
+}
+
 
 //--------------------------------------------------------------
 //|                     Klasa Producent                        |
 //--------------------------------------------------------------
+Producent::Producent(int id, string na )
+{
+   this ->id = id;
+    name = na;
+}
+void Producent::show()
+{
+    cout << id << ". " << name << endl;
+}
+string Producent::get_name()
+{
+        return name;
+}
+int Producent::get_id()
+{
+    return id;
+}
+void Producent::gname()
+{
+    cout << name;
+}
 
 //--------------------------------------------------------------
 //|                     Klasa Warehouse                        |
@@ -112,10 +428,32 @@ void Graphic_Card::open()
 
 void Warehouse::Towar()
 {
+
     char a;
+    cout << "===============Procesory================" << endl;
     for (int i = 0; i < procesory.size(); i++)
     {
-        procesory[i].show_all();
+         procesory[i].show_all();
+    }
+    cout << endl << "=============Karty graficzne=============" << endl;
+    for (int i = 0; i < karty.size(); i++)
+    {
+        karty[i].show_all();
+    }
+    cout << endl << "=================Pamiêæ RAM===================" << endl;
+    for (int i = 0; i < ramy.size(); i++)
+    {
+        ramy[i].show_all();
+    }
+    cout << endl << "================P³yty g³ówne======================" << endl;
+    for (int i = 0; i < plyty.size(); i++)
+    {
+        plyty[i].show_all();
+    }
+    cout << endl << "======================Zasilacze======================" << endl;
+    for (int i = 0; i < zasilacze.size(); i++)
+    {
+        zasilacze[i].show_all();
     }
     cout << "Chcesz kontynuowaæ?";
     cin >> a;
@@ -124,11 +462,46 @@ void Warehouse::Towar()
 }
 void Warehouse::Stan_magazynowy()
 {
-
+    cout << "===============Procesory================" << endl;
+    for (int i = 0; i < procesory.size(); i++)
+    {
+        procesory[i].show_min();
+    }
+    cout << endl << "=============Karty graficzne=============" << endl;
+    for (int i = 0; i < karty.size(); i++)
+    {
+        karty[i].show_min();
+    }
+    cout << endl << "=================Pamiêæ RAM===================" << endl;
+    for (int i = 0; i < ramy.size(); i++)
+    {
+        ramy[i].show_min();
+    }
+    cout << endl << "================P³yty g³ówne======================" << endl;
+    for (int i = 0; i < plyty.size(); i++)
+    {
+        plyty[i].show_min();
+    }
+    cout << endl << "======================Zasilacze======================" << endl;
+    for (int i = 0; i < zasilacze.size(); i++)
+    {
+        zasilacze[i].show_min();
+    }
 }
 void Warehouse::Producenci()
 {
-
+    char a;
+    do
+    {
+        for (int i = 0; i < producenci.size(); i++)
+        {
+            producenci[i].show();
+        }
+        cout << "Czy chcesz wróciæ do menu g³ównego? T/N" << endl;
+        cin >> a;
+        system("cls");
+    } while (a == 'T');
+    Main_Menu();
 }
 void Warehouse::Dostawa()
 {
@@ -136,8 +509,9 @@ void Warehouse::Dostawa()
 }
 void Warehouse::Add_new_product()
 {
-    int n, vr;
-    string nazwa;
+    int n,m,pr, vr;
+    int temp;
+    string nazwa,temp1,temp2;
     char a;
     float mh;
     do
@@ -149,37 +523,112 @@ void Warehouse::Add_new_product()
         cout << "3.Dysk" << endl;
         cout << "4.Koœci RAM" << endl;
         cout << "5.P³yta g³ówna" << endl;
+        cout << "6.Zasilacz" << endl;
         cin >> n;
-    
-        if (0 < n < 6)
+        if (n < 7 || n > 1)
         {
             switch (n)
             {
             case 1:
-                cout << "Podaj nazwe ";
+                cout << "Podaj nazwe "<<endl;
                 getline(cin >> ws, nazwa);
-                cout << "Podaj Mhz";
+                cout << "Podaj Mhz"<<endl;
                 cin >> mh;
+                for (int i = 0; i < producenci.size(); i++)
+                {
+                    producenci[i].show();
+                }
+                cout << endl << "Wybierz producenta. " << endl;
+                cin >> pr;
                 system("cls");
-                procesory.push_back(Processor(nazwa, pid, mh));
-                pid++;
+                procesory.push_back(Processor(nazwa, prid, mh, zero, pr));
+                prid++;
                 cout << "Pomyœlnie dodano nowy produkt";
                 break;
 
             case 2:
-                cout << "Podaj nazwe."<<endl;
-                cin >> nazwa;
-                cout << "Podaj VRAM"<<endl;
-                cin >> mh;
-                system("cls");
-                karty.push_back(Graphic_Card(nazwa, pid, mh));
-                pid++;
+                cout << "Podaj nazwe: " << endl;
+                getline(cin >> ws, nazwa);
+                cout << "Podaj iloœæ VRAM: " << endl;
+                cin >> vr;
+                for (int i = 0; i < producenci.size(); i++)
+                {
+                    producenci[i].show();
+                }
+                cout << endl << "Wybierz producenta. " << endl;
+                cin >> pr;
+                karty.push_back(GraphicCards(nazwa, vr, grid, zero, pr));
+
+                grid++;
+                cout << "Pomyœlnie dodano nowy produkt";
+                break;
+            case 3:
+                cout << "Podaj nazwe: " << endl;
+                getline(cin >> ws, nazwa);
+                cout << "Podaj pojemnoœæ: " << endl;
+                cin >> temp;
+                for (int i = 0; i < producenci.size(); i++)
+                {
+                    producenci[i].show();
+                }
+                cout << endl << "Wybierz producenta. " << endl;
+                cin >> pr;
+                dyski.push_back(Hard_Drive(nazwa, temp, haid, pr, zero));
+                haid++;
+                cout << "Pomyœlnie dodano nowy produkt";
+                break;
+            case 4:
+                cout << "Podaj nazwe: " << endl;
+                getline(cin >> ws, nazwa);
+                cout << "Podaj iloœæ GB: " << endl;
+                cin >> vr;
+                cout << "Podaj taktowanie: " << endl;
+                cin >> temp;
+                for (int i = 0; i < producenci.size(); i++)
+                {
+                    producenci[i].show();
+                }
+                cout << endl << "Wybierz producenta. " << endl;
+                cin >> pr;
+                ramy.push_back(RAM_memory(nazwa, vr, temp, ramid, pr));
+                ramid++;
+                cout << "Pomyœlnie dodano nowy produkt";
+                break;
+            case 5:
+                cout << "Podaj nazwe: " << endl;
+                getline(cin >> ws, nazwa);
+                cout << "Podaj format p³yty: " << endl;
+                getline(cin >> ws, temp1);
+                cout << "Podaj socket p³yty: " << endl;
+                getline(cin >> ws, temp2);
+                for (int i = 0; i < producenci.size(); i++)
+                {
+                    producenci[i].show();
+                }
+                cout << endl << "Wybierz producenta. " << endl;
+                cin >> pr;
+                plyty.push_back(Motherboard(nazwa, temp1, temp2, zero, motid, pr));
+                motid++;
+                cout << "Pomyœlnie dodano nowy produkt";
+                break;
+            case 6:
+                cout << "Podaj nazwe: " << endl;
+                getline(cin >> ws, nazwa);
+                cout << "Podaj moc: " << endl;
+                cin >> temp;
+                for (int i = 0; i < producenci.size(); i++)
+                {
+                    producenci[i].show();
+                }
+                cout << endl << "Wybierz producenta. " << endl;
+                cin >> pr;
+                zasilacze.push_back(Power_Supply(nazwa, temp, zero, pr, powid));
+                powid++;
                 cout << "Pomyœlnie dodano nowy produkt";
                 break;
             }
-            //Reszta potem//
         }
-        if(n>6)
+        if(n>7)
         {
             cout << "Rz¹dana opcja nie istnieje"<<endl;
         }
@@ -192,7 +641,14 @@ void Warehouse::Add_new_product()
 }
 void Warehouse::Add_new_producent()
 {
-
+    string nazwa;
+    cout << "Podaj nazwê producenta " << endl;
+    getline(cin >> ws, nazwa);
+    producenci.push_back(Producent(prodid, nazwa));
+    prodid++;
+    system("cls");
+    cout << "Dodano pomyœlnie producenta";
+    Main_Menu();
 }
 void Warehouse::Main_Menu()
 {
@@ -245,14 +701,32 @@ void Warehouse::Main_Menu()
 void Warehouse::end()
 {
     ofstream zapis("procesory.txt");
-
     for(int i = 0; i < procesory.size(); i++)
     {
         zapis << procesory[i].get_id()<<endl;
-        zapis << procesory[i].namee()<<endl;
+        zapis << procesory[i].get_name()<<endl;
         zapis << procesory[i].M()<<endl;
+        zapis << procesory[i].get_idpr() << endl;
         zapis << procesory[i].amm() << endl;
 
     }
     zapis.close();
+
+    ofstream prod("producenci.txt");
+    for (int i = 0; i < producenci.size(); i++)
+    {
+        prod << producenci[i].get_id() << endl;
+        prod << producenci[i].get_name() << endl;
+    }
+    prod.close();
+    ofstream kart("karty_graficzne.txt");
+    for (int i = 0; i < karty.size(); i++)
+    {
+        kart << karty[i].get_id();
+        kart << karty[i].get_name();
+        kart << karty[i].get_Vram();
+        kart << karty[i].get_idp();
+        kart << karty[i].get_am();
+    }
+    kart.close();
 }
--- Warehouse/Class.h
+++ Warehouse/Class.h
@@ -14,76 +14,133 @@ public:
 
     void show();
     int get_id();
+    virtual void add_amm(int t) = 0;
+    virtual void rem_amm(int t) = 0;
 };
+//Klasa z danymi producenta
+class Producent
+{
+    int id;
+    string name;
+public:
+    Producent(int id = 0, string na = "none");
 
+    void show();
+    int get_id();
+    void gname();
+    string get_name();
+
+};
 //Klasa procesora
 class Processor : public Computer_Parts
 {
     string name;
-    int amount ;
+    int amount, idpr;
     float MHz;
 public:
-    Processor(string n = "s", int i = 0, double MHz = 1, int amm = 0);
+    Processor(string n = "s", int i = 0, float MHz = 1.0, int amm = 0,int idpr = 0);
 
     void show();
     void show_all();
     void show_min();
-    int idd();
-    string namee();
+    string get_name();
     int amm();
     float M();
-   
+    int get_idpr();
+    virtual void add_amm(int t);
+    virtual void rem_amm(int t);
 };
 
 //Klasa karty graficznej
-class Graphic_Card : public Computer_Parts
+class GraphicCards : public Computer_Parts, public Producent
 {
     string name;
-    int Vram,amount;
+    int Vram,amount,idp;
 public:
-    Graphic_Card(string n = "s", int rr = 0, int id = 0,int am = 0);
+    GraphicCards(string n = "s", int rr = 0, int id = 0,int am = 0,int id2 = 0);
 
     void show();
-    void save();
-    void open();
- 
+    void show_all();
+    void show_min();
+    string get_name();
+    int get_am();
+    int get_Vram();
+    int get_idp();
+    virtual void add_amm(int t);
+    virtual void rem_amm(int t);
 };
 
 //Klasa pamiêci RAM
 class RAM_memory : public Computer_Parts
 {
     string name;
+    int GB, Takt,prod,amount;
+public:
+    RAM_memory(string nam = "s", int g = 0, int T = 0, int id = 0,int idp=0);
+
+    void show_all();
+    void show_min();
+    string get_name();
+    int get_GB();
+    int get_Tac();
+    int get_prod();
+    int get_amo();
+    virtual void add_amm(int t);
+    virtual void rem_amm(int t);
 };
 
 //Klasa p³yty g³ownej
 class Motherboard : public Computer_Parts
 {
-    string name;
+    string name,format,socket;
+    int amount,prod;
+public:
+    Motherboard(string nam = "s", string forr = "ss", string soc = "sss", int amo = 0,int id1 = 0,int id2=0);
+
+    void show_all();
+    void show_min();
+    string get_name();
+    string get_form();
+    string get_sock();
+    int get_amo();
+    int get_prod();
+    virtual void add_amm(int t);
+    virtual void rem_amm(int t);
 };
 
 //Klasa zasilacza
 class Power_Supply : public Computer_Parts
 {
     string name;
-};
-
-//Klasa z danymi producenta
-class Producent
-{
-    int id;
-    string name, adress;
+    int Power,amount,prod;
 public:
-    Producent(int id, string na, string ad)
-    {
-        id = id;
-        name = na;
-        adress = ad;
-    }
-    void show()
-    {
-        cout << "Id Producenta: " << id << endl;
-        cout << "Nazwa: " << name << ". Adres: " << adress << endl;
-    }
+    Power_Supply(string nam ="n", int Pow = 0, int am = 0, int pid = 0, int id1 = 0);
+
+    void show_all();
+    void show_min();
+    string get_name();
+    int get_powder();
+    int get_am();
+    int get_prod();
+    virtual void add_amm(int t);
+    virtual void rem_amm(int t);
+};
+//klasa dysków twardych
+class Hard_Drive : public Computer_Parts
+{
+    string name;
+    int poj, amount, prod;
+public:
+    Hard_Drive(string nam = "n", int po = 0, int id1 = 0, int pid = 0, int am = 0);
+    void show_all();
+    void show_min();
+    string power();
+    int get_poj();
+    int get_amo();
+    int get_prod();
+    virtual void add_amm(int t);
+    virtual void rem_amm(int t);
+
 };
 //Klasa magazynu
 class Warehouse 
